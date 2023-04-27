1e2b252b6897f0066ff2d53e121f8ec1e64ced69
code <joycelu910206@gmail.com>
2023-04-28 03:06:31 +0800

    deploy

--- app/utils/instances.js
+++ app/utils/instances.js
@@ -1,14 +1,13 @@
 import postAbi from './Post.json'
 import factoryAbi from './BlogFactory.json'
 
-const contractAddress = '0x5C61734441fe9F06Ffc7BE970D4a766B79bFcF85'
-
+const contractAddress = '0x28Cf6C6a540864DaA392A125d9a233fA17BdC8e8'
 const blogInstance = (web3Instance, address) => {
   return new web3Instance.eth.Contract(postAbi.abi, address)
 }
 
 const factoryInstance = web3Instance => {
-  return new web3Instance.eth.Contract(factoryAbi.abi, contractAddress)
+  return new web3Instance.eth.Contract(factoryAbi.abi, address)
 }
 
 export { blogInstance, factoryInstance }
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
+const mnemonic ="chunk double token plunge fetch across real toe box review lesson sad";
 module.exports = {
   mocha: {},
   compilers: {
@@ -9,14 +10,14 @@ module.exports = {
   },
   networks: {
     loc_development_development: {
-      network_id: "*",
+      network_id:"5777",
       port: 7545,
       host: "127.0.0.1"
     },
-    "inf_solidity-blog-app_goerli": {
+    inf_solidity_blog_app_goerli: {
       network_id: 5,
       gasPrice: 100000000000,
-      provider: new HDWalletProvider(fs.readFileSync('/Users/davidrakosi/dev-wallet/dev-wallet.env', 'utf-8'), "https://goerli.infura.io/v3/84356f3bc0d14e959770cbb97274c912")
+      provider: new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/6d5f676342d34495bdf97a5caa0c5ea8")
     }
   }
 };
